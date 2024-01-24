1f0e96e47ae1d4fbc6e8dec3c6cda8e8fae72935
Ei Myat <74184859+eimyat528@users.noreply.github.com>
2024-01-24 14:59:25 +0630

    week 3

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index']);
+
+
+
+// Route::get('/', function () {
+//     $data = [
+//         'home_key' => 'home_value',
+//         // 'key2' => 'value2',
+//         // 'key3' => 'value3',
+//     ];
+//     return view('home',compact('data'));
+// });
+
+// Route::get('/contact', function () {
+//     $data = [
+//         'contact_key' => 'contact_value',
+      
+//     ];
+//     return view('contact',compact('data'));
+// });
+
+// Route::get('/about', function () {
+//     $data = [
+//         'about_key' => 'about_value',
+      
+//     ];
+//     return view('about',compact('data'));
+// });
+
